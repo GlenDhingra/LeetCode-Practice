1615188759c33d94fc37fccad1a98ecf7d654ab4
GlenDhingra <50907464+GlenDhingra@users.noreply.github.com>
2024-10-15 09:50:17 -0700

    Cheapest Fights with K stops

--- LeetCode_787.java
+++ LeetCode_787.java
@@ -1,78 +1,63 @@
+import java.util.*;
+
 class Solution {
     public int findCheapestPrice(int n, int[][] flights, int src, int dst, int k) {
         int[] costToReachNode = new int[n];
-        for( int i = 0 ; i < costToReachNode.length ; i++ ){
-            costToReachNode[i] = 100001;
-        }
+        Arrays.fill(costToReachNode, Integer.MAX_VALUE);
         costToReachNode[src] = 0;
-        HashMap<Integer, ArrayList<Destination>> map = new HashMap<>();
-        for( int i = 0 ; i < flights.length ; i++ ){
-            if(!map.containsKey(flights[i][0])){
-                map.put(flights[i][0], new ArrayList<Destination>());
-            }  
-            ArrayList<Destination> temp = map.get(flights[i][0]);
-            Destination object = new Destination(flights[i][1], flights[i][2]);
-            temp.add(object);
-            map.put(flights[i][0], temp);
+
+        HashMap<Integer, List<Destination>> map = new HashMap<>();
+        for (int[] flight : flights) {
+            map.putIfAbsent(flight[0], new ArrayList<>());
+            map.get(flight[0]).add(new Destination(flight[1], flight[2]));
         }
-        Queue<Integer> queue = new LinkedList<>();
-        queue.add(src);
-        int count = 0;
-        while( !queue.isEmpty() && count <= k){
-            int size = queue.size();
-            count++;
-            int tempCostToReachNode[] = Arrays.copyOf(costToReachNode, costToReachNode.length);
-            for( int j = 0 ; j < size ; j++ ){
-                int node = queue.poll();
-                
-                if( map.get(node) == null ){
-                    continue;
-                }
-                ArrayList<Destination> destinations = map.get(node);
-                
-                for( int i = 0 ; i < destinations.size() ; i++ ){
-                    
-                    Destination current = destinations.get(i);
-                    int dest = current.getDest();
-                    
-                    int cost = current.getCost();
-                    int prevDestCost = tempCostToReachNode[dest];
-                    int prevSourceCost = costToReachNode[node];
-                    System.out.println(prevDestCost);
-                    tempCostToReachNode[dest] = Math.min(prevSourceCost + cost, prevDestCost);
-                    System.out.println(node + " > " + dest + " = " + tempCostToReachNode[dest]);
-                    queue.add(dest);
+        Queue<int[]> queue = new LinkedList<>();
+        queue.add(new int[]{src, 0, 0});
+        
+        int minCost = Integer.MAX_VALUE;
+
+        while (!queue.isEmpty()) {
+            int[] current = queue.poll();
+            int node = current[0];
+            int currentCost = current[1];
+            int stops = current[2]; 
+
+            if (node == dst) {
+                minCost = Math.min(minCost, currentCost);
+                continue;
+            }
+            if (stops > k) {
+                continue;
+            }
+            if (map.containsKey(node)) {
+                for (Destination destination : map.get(node)) {
+                    int nextNode = destination.getDest();
+                    int price = destination.getCost();
+                    int newCost = currentCost + price;
+                    if (newCost < costToReachNode[nextNode]) {
+                        costToReachNode[nextNode] = newCost;
+                        queue.add(new int[]{nextNode, newCost, stops + 1});
+                    }
                 }
             }
-            costToReachNode = Arrays.copyOf(tempCostToReachNode, tempCostToReachNode.length);
         }
-        return costToReachNode[dst] > 100000 ? -1 : costToReachNode[dst];
+        return minCost == Integer.MAX_VALUE ? -1 : minCost;
     }
 }
 
-class Destination{
+class Destination {
     int dest, cost;
-    public Destination(int dest, int cost){
+
+    public Destination(int dest, int cost) {
         this.dest = dest;
         this.cost = cost;
     }
-    public void setDest(int dest){
-        this.dest = dest;
-    }
-    public void setCost(int cost){
-        this.cost = cost;
-    }
-    public int getDest(){
+
+    public int getDest() {
         return dest;
     }
-    public int getCost(){
+
+    public int getCost() {
         return cost;
     }
 }
-/*
-class
-class
-hashmap
-queue
-k=stop, consider least till k
-*/
