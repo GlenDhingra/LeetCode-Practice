c40b9cc0df1a9ed1597ced9e2b6ccff766917511
GlenDhingra <50907464+GlenDhingra@users.noreply.github.com>
2024-10-18 18:24:20 -0700

    Kth largest element

--- LeetCode_215.java
+++ LeetCode_215.java
@@ -1,8 +1,17 @@
-class Solution 
-{
-    public int findKthLargest(int[] nums, int k) 
-    {
-        Arrays.sort(nums);
-        return nums[ nums.length - k ];
+class Solution {
+    public int findKthLargest(int[] nums, int k) {
+        PriorityQueue<Integer> queue = new PriorityQueue<>();
+        for( int i = 0 ; i < nums.length ; i++ ){
+            if( queue.size() < k ){
+                queue.add(nums[i]);
+            }
+            else{
+                if( nums[i] > queue.peek() ){
+                    queue.poll();
+                    queue.add(nums[i]);
+                }
+            }
+        }     
+        return queue.peek();
     }
 }
