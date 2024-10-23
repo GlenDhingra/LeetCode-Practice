995b9c17467f0acbe1a2c19f2c0e03006f3219e5
GlenDhingra <50907464+GlenDhingra@users.noreply.github.com>
2024-10-23 12:28:38 -0700

    Valid Palindrome

--- LeetCode_125.java
+++ LeetCode_125.java
@@ -1,15 +1,18 @@
-class Solution 
-{
-    public boolean isPalindrome(String s) 
-    {
-        int i = 0;
-        int j = s.length() - 1;
+class Solution {
+    public boolean isPalindrome(String s) {
         s = s.toLowerCase();
-        while(i < j)
-        {
-            while( i < j && !Character.isLetterOrDigit(s.charAt(i)) ) i++;
-            while( j > i && !Character.isLetterOrDigit(s.charAt(j)) ) j--;
-            if( s.charAt(i) != s.charAt(j) ) return false;
+        int i = 0;
+        int j = s.length()-1;
+        while( i < j ){
+            while( i < j && ( !Character.isDigit(s.charAt(i)) && !Character.isAlphabetic(s.charAt(i) ))  ){
+                i++;
+            }
+            while( j > i && ( !Character.isDigit(s.charAt(j)) && !Character.isAlphabetic(s.charAt(j) ))  ){
+                j--;
+            }
+            if( i < j && s.charAt(i) != s.charAt(j) ){
+                return false;
+            }
             i++;
             j--;
         }
